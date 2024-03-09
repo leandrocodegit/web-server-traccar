c3b5c7d3dc8edcc9546da88c4e0e7b2da99875fd
leandrocodegit <lpoliveira.ti@gmail.com>
2024-03-09 08:36:17 -0300

    Salvando projeto

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,28 +3,76 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
+use Illuminate\Http\Exceptions\PostTooLargeException;
 use Throwable;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Validation\ValidationException;
+use Maatwebsite\Excel\Exceptions\NoTypeDetectedException;
+use InvalidArgumentException;
+use Exception;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Str;
+
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
+
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
+    public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'errors' => array('Recurso não encontrado!'),
+                ], 404);
+            }else{
+                return response(view('angular'), 200);
+            }
         });
+
+        $this->renderable(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'errors' => array('Recurso não encontrado!')
+                ], 404);
+            }
+        });
+
+        $this->renderable(function (QueryException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'errors' => array('Falha na requisição!', $e->errorInfo),
+                    'codigo' => $e->getCode()
+                ], 404);
+            }
+        });
+
+
+        $this->renderable(function (InvalidArgumentException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'errors' => array('Formato inválido!')
+                ], 404);
+            }
+        });
+
+        $this->renderable(function (ValidationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'errors' => array('A requisição não foi processada!')
+                ], 422);
+            }
+        });
+
+ 
+
     }
 }
